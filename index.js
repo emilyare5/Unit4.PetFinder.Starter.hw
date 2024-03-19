659227151b57c3d6bcbe7642bb1677dc4cbec3a6
emilyare5 <emilyarellano1819@gmail>
2024-03-19 18:41:46 -0400

    code

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const app = express();
 
 const PORT = 8080;
 
+const path = require('path');
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
 
+    // res.sendFile() is used to send the index.html file to the client
+    // __dirname: This is a Node.js global variable that represents the directory name of the current module (i.e., the directory in which this script resides).
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
 // hello world route
@@ -20,31 +24,47 @@ app.get('/api', (req, res) => {
 
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
+    res.send(pets)
     // send the pets array as a response
 
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
+    // res.send("pet by owner name")
     // get the owner from the request
+    const {owner} = req.query
+    // console.log(owner)
+    // res.send(owner)
+
+    // const owner = req.query.owner
+    // /api/v1/pets/owner?owner=John&pet=Fido
+    
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
-    // send the pet as a response
+    res.send(pet)
+    // console.log(pet)
+    // http://localhost:8081/api/v1/pets/owner?owner=John
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
+    // res.send("pet by name")
     // get the name from the request
+   const name = req.params.name
+ //    {name}=req.params
+    
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
-    // send the pet as a response
+    res.send(pet)
+    // http://localhost:8080/api/v1/pets/Fido
 
 });
 
